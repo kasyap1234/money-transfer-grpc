1b00d4b3354708cb8b5bd2cd3c3e3758ce06f75b
tgt <kasyap3103@gmail.com>
2024-12-16 12:21:41 +0530

    second commit

---
+++ transfer-server.go
@@ -0,0 +1,15 @@
+package main 
+
+import (
+	"context"
+	"fmt"
+	pb "github.com/kasyap1234/money-transfer-grpc/transfer"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
+
+)
+
+
+
